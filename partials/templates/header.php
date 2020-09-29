00ab6140143333c422db7b7571055fa018c9492c
Guido <guido.amodio@gmail.com>
2020-09-29 13:14:38 +0200

    CRUD

---
+++ partials/templates/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="dist/app.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP CRUD</title>
+    </head>
